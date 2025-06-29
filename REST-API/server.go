eec4cd732462d401f6bf32352d30ed38bde2e278
Sulabh Ghimire <sulabhghimire82@gmail.com>
2025-06-29 21:36:49 +0545

    How HTTP/1.1 works

--- REST-API/server.go
+++ REST-API/server.go
@@ -12,10 +12,12 @@ import (
 func main() {
 
 	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
 		fmt.Fprintf(w, "Handling incoming orders.")
 	})
 
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
 		fmt.Fprintf(w, "Handling users.")
 	})
 
@@ -63,4 +65,31 @@ func main() {
 	// 	log.Fatalln("Couldn't start the server:", err)
 	// }
 
+}
+
+func logRequestDetails(r *http.Request) {
+	httpVersion := r.Proto
+	fmt.Println("Received request with HTTP Version:", httpVersion)
+
+	if r.TLS != nil {
+		tlsVersion := getTLSVersionName(r.TLS.Version)
+		fmt.Println("Received request with TLS Version:", tlsVersion)
+	} else {
+		fmt.Println("Received request without TLS")
+	}
+}
+
+func getTLSVersionName(version uint16) string {
+	switch version {
+	case tls.VersionTLS10:
+		return "TLS 1.0"
+	case tls.VersionTLS11:
+		return "TLS 1.1"
+	case tls.VersionTLS12:
+		return "TLS 1.2"
+	case tls.VersionTLS13:
+		return "TLS 1.3"
+	default:
+		return "Unknown TLS Version"
+	}
 }
